0fda385435ae16df9ee62cd2dfae07c1e85be58d
Mukhtar Fauzi <mukhtar.fz@gmail.com>
2024-03-27 15:38:12 +0700

    export data class

--- lib/editorjs_flutter.dart
+++ lib/editorjs_flutter.dart
@@ -2,3 +2,10 @@ library editorjs_flutter;
 
 export 'src/widgets/view.dart';
 export 'src/widgets/editor.dart';
+export 'src/model/EditorJSBlock.dart';
+export 'src/model/EditorJSBlockData.dart';
+export 'src/model/EditorJSBlockFile.dart';
+export 'src/model/EditorJSCSSTag.dart';
+export 'src/model/EditorJSData.dart';
+export 'src/model/EditorJSViewStyles.dart';
+
